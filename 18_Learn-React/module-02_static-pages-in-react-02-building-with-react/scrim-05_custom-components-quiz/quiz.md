d5bfb403e3e3f201ece248aaadaf9eca9d2820a8
Ibrahim Abdullah <iabdullah012@gmail.com>
2025-12-10 03:25:44 +0500

    Completed | Learn React | Module 02

---
+++ 18_Learn-React/module-02_static-pages-in-react-02-building-with-react/scrim-05_custom-components-quiz/quiz.md
@@ -0,0 +1,27 @@
+1. What is a React component?
+A function that returns React elements. (UI)
+
+2. What's wrong with this code?
+```
+function myComponent() {
+    return (
+        <small>I'm tiny text!</small>
+    )
+}
+```
+
+Correct answer: The function name should start with a capital letter: MyComponent.
+
+3. What's wrong with this code?
+```
+function Header() {
+    return (
+        <header>
+            <img src="./react-logo.png" width="40px" alt="React logo" />
+        </header>
+    )
+}
+
+root.render(Header());
+```
+Correct answer: The component should be rendered as a JSX element: <Header />.
---
+++ 18_Learn-React/module-02_static-pages-in-react-02-building-with-react/scrim-08_styling-with-classes/index.jsx
@@ -0,0 +1,60 @@
+import { createRoot } from "react-dom/client"
+const root = createRoot(document.getElementById("root"))
+
+/*
+    Why className instead of class?
+
+    const ul = document.createElement("ul")
+    ul.className = ""
+
+    When creating elements with JavaScript, we use the property "className" to set the class attribute of an element.
+*/
+
+function Header() {
+    return (
+        <header className="header">
+            <img src="react-logo.png" className="nav-logo" alt="React logo" />
+            <nav>
+                <ul className="nav-list">
+                    <li className="nav-list-item">Pricing</li>
+                    <li className="nav-list-item">About</li>
+                    <li className="nav-list-item">Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function MainContent() {
+    return (
+        <main>
+            <h1>Reason I am excited to learn React</h1>
+            <ol>
+                <li>React is a popular library, so I will be able to fit in with all the coolest devs out there! 😎</li>
+                <li>I am more likely to get a job as a front end developer if I know React</li>
+            </ol>
+        </main>
+    )
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2024 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function Page() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    )
+}
+
+root.render(
+    <Page />
+)
