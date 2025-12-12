f04a275683ae9509a5765b2f299de2006d30daa8
Ibrahim Abdullah <iabdullah012@gmail.com>
2025-12-13 03:23:30 +0500

    Completed | Learn React | Module 07

---
+++ 18_Learn-React/module-07_react-state-01-introduction-and-basics/scrim-05_event-listeners/index.jsx
@@ -0,0 +1,25 @@
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  
+  function handleClick() {
+    console.log("I was clicked!")
+  }
+  
+  function handleMouseOver() {
+    console.log("I was hovered!")
+  }
+  
+  return (
+    <main className="container">
+      <img
+        src="https://picsum.photos/640/360"
+        alt="Placeholder image from Picsum"
+        onMouseOver={handleMouseOver}
+      />
+      <button onClick={handleClick}>Click me</button>
+    </main>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
