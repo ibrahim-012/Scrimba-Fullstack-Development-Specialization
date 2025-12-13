be887dd2144f5f4e51e3636bd213eb934e707cfe
Ibrahim Abdullah <iabdullah012@gmail.com>
2025-12-13 10:42:09 +0500

    Completed | Learn React | Module 09

---
+++ 18_Learn-React/module-09_conditional-rendering-and-state-communication/scrim-02_conditional-rendering-&&/Joke.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+export default function Joke(props) {
+    const [isShown, setIsShown] = React.useState(false)
+    
+    function toggleShown() {
+        setIsShown(prevShown => !prevShown)
+    }
+    
+    /**
+     * Challenge:
+     * Only display the punchline paragraph if `isShown` is true
+     */
+    
+    return (
+        <div>
+            {props.setup && <h3>{props.setup}</h3>}
+            {isShown && <p>{props.punchline}</p>}
+            <button onClick={toggleShown}>Show punchline</button>
+            <hr />
+        </div>
+    )
+}
---
+++ 18_Learn-React/module-09_conditional-rendering-and-state-communication/scrim-11_passing-state-as-props/App.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+import Count from "./Count"
+
+export default function App() {
+    const [count, setCount] = React.useState(0)
+
+    function add() {
+        setCount(prevCount => prevCount + 1)
+    }
+
+    function subtract() {
+        setCount(prevCount => prevCount - 1)
+    }
+    
+    console.log("App rendered")
+    
+
+    return (
+        <main className="container">
+            <div className="counter">
+                <button
+                    className="minus"
+                    onClick={subtract}
+                    aria-label="Decrease count"
+                >-</button>
+
+                <Count number={count} />
+
+                <button
+                    className="plus"
+                    onClick={add}
+                    aria-label="Increase count"
+                >+</button>
+            </div>
+        </main>
+    )
+}
