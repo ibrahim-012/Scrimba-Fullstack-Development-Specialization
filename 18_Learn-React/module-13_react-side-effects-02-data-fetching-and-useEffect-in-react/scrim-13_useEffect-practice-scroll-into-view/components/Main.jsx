1c11a8c2ac72e35ff6e5e8cdd5cfae618299af00
Ibrahim Abdullah <iabdullah012@gmail.com>
2025-12-14 12:39:30 +0500

    Completed | Learn React | Module 13

---
+++ 18_Learn-React/module-13_react-side-effects-02-data-fetching-and-useEffect-in-react/scrim-13_useEffect-practice-scroll-into-view/components/Main.jsx
@@ -0,0 +1,73 @@
+import React from "react"
+import IngredientsList from "./IngredientsList"
+import ClaudeRecipe from "./ClaudeRecipe"
+import { getRecipeFromChefClaude, getRecipeFromMistral } from "../ai"
+
+export default function Main() {
+    const [ingredients, setIngredients] = React.useState(
+        ["chicken", "all the main spices", "corn", "heavy cream", "pasta"]
+    )
+    const [recipe, setRecipe] = React.useState("")
+
+
+    /**
+         * Problem:
+         * We want to scroll the "Ready for a recipe?" div into view
+         * ONLY AFTER the ClaudeRecipe section is rendered to the page 
+         * (i.e. when `recipe` is not an empty string). How can we do that?
+     */
+
+    /**
+         * Challenge:
+         * Add a new effect that calls `recipeSection.current.scrollIntoView()`
+         * only if recipe is not an empty string and recipeSection.current is not null.
+         * Think carefully about what value(s) you would want to include in
+         * the dependencies array.
+     */
+    const recipeSection = React.useRef(null)
+    
+    React.useEffect(() => {
+        if (recipe !== "" && recipeSection.current !== null) {
+            // recipeSection.current.scrollIntoView({behavior: "smooth"})
+            const yCoord = recipeSection.current.getBoundingClientRect().top + window.scrollY
+            window.scroll({
+                top: yCoord,
+                behavior: "smooth"
+            })
+        }
+    }, [recipe])
+
+    async function getRecipe() {
+        const recipeMarkdown = await getRecipeFromChefClaude(ingredients)
+        setRecipe(recipeMarkdown)
+    }
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+    
+    return (
+        <main>
+            <form action={addIngredient} className="add-ingredient-form">
+                <input
+                    type="text"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
+                />
+                <button>Add ingredient</button>
+            </form>
+
+            {ingredients.length > 0 &&
+                <IngredientsList
+                    ref={recipeSection}
+                    ingredients={ingredients}
+                    getRecipe={getRecipe}
+                />
+            }
+
+            {recipe && <ClaudeRecipe recipe={recipe} />}
+        </main>
+    )
+}
