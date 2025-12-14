f07d3e24821a510ff906f8b9bd49a1468786ff73
Ibrahim Abdullah <iabdullah012@gmail.com>
2025-12-14 10:36:02 +0500

    Completed | Learn React | Module 12

---
+++ 18_Learn-React/module-12_react-side-effects-01-building-the-meme-generator-and-managing-state/scrim-05_meme-generator-controlled-components-part-2/components/Main.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react"
+
+export default function Main() {
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+    
+    function handleChange(event) {
+        const {value, name} = event.currentTarget
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+    /*
+        Why we use [name] instead of name:
+
+        - Writing:  name: value
+        → creates/updates a property literally called "name"
+
+        - Writing:  [name]: value
+        → uses the VALUE stored in the variable `name` as the key
+
+        Example:
+        If name === "topText", then:
+        [name]: value
+        becomes:
+        topText: value
+
+        This allows ONE onChange handler to update
+        different state properties based on the input's name attribute.
+    */
+
+
+    return (
+        <main>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        onChange={handleChange}
+                        value={meme.topText}
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        onChange={handleChange}
+                        value={meme.bottomText}
+                    />
+                </label>
+                <button>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    )
+}
